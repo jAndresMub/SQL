a37d690299e51a13af37a915bbf2df35f22dfd8d
jandresmub <jandresmub@gmail.com>
2019-09-01 20:31:11 -0500

    final del curso

--- sql.sql
+++ sql.sql
@@ -64,4 +64,82 @@ VALUES ('Julio Cortazar','ARG'),
        ('Octavio Paz','MEX'),
        ('Juan Carlos Onetti','URU');
 
-#SELECT name FROM authors WHERE nationality = 'COL';
+SELECT name, email, YEAR(NOW()) - YEAR(birthdate) AS Edad, gender
+FROM clients
+WHERE gender = 'F'
+    AND name LIKE '%Lop%';
+
+SELECT count(*) FROM books
+
+SELECT b.book_id, a.name, b.title
+FROM books as b
+join authors as a 
+    on a.author_id = b.author_id
+where a.author_id between 1 and 5;
+
+
+SELECT c.name, b.title, a.name, t.type
+from operations as t
+join books as b 
+    on t.book_id = b.book_id
+join clients as c
+    on t.client_id = c.client_id
+join authors as a
+    on b.author_id = a.author_id
+where c.gender = 'M'
+    and t.type IN ('sell', 'lend')
+order by a.author_id;
+
+
+SELECT a.author_id, a.name, a.nationality, COUNT(b.book_id)
+FROM authors as a
+LEFT JOIN books as b
+    ON b.author_id = a.author_id
+WHERE a.author_id between 1 and 5
+GROUP BY a.author_id
+ORDER BY a.author_id;
+
+
+SELECT a.author_id, a.name, a.nationality, COUNT(b.book_id) as 'cantidad de libros'
+FROM authors as a
+LEFT JOIN books as b
+    ON b.author_id = a.author_id
+WHERE a.author_id
+GROUP BY a.author_id
+ORDER BY a.author_id;
+
+SELECT DISTINCT nationality FROM authors;
+
+SELECT count(DISTINCT nationality) FROM authors;
+
+SELECT nationality, COUNT(author_id) as c_authors
+FROM authors
+GROUP BY nationality
+ORDER BY c_authors DESC;
+
+
+SELECT nationality, COUNT(author_id) as c_authors
+FROM authors
+WHERE nationality IS NOT NULL
+GROUP BY nationality
+ORDER BY c_authors DESC;
+
+SELECT AVG(price)
+FROM books;
+
+SELECT MAX(price), MIN(price)
+FROM books;
+
+SELECT count(book_id),
+    sum(if(`year` < 1950, 1, 0)) as `<1950`,
+    sum(if(`year` < 1950, 0, 1)) as `>1950`
+from books;
+
+SELECT nationality, count(book_id),
+    sum(if(`year` < 1950, 1, 0)) as `<1950`,
+    sum(if(`year` < 1950, 0, 1)) as `>1950`
+from books as b
+join authors as a
+    on a.author_id = b.author_id
+where a.nationality is not NULL
+GROUP BY nationality;
